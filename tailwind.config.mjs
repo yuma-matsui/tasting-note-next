80080bc662264a16758a7759840aa15801499ca6
yuma-matsui <oshino.iiyo@gmail.com>
2023-08-08 22:34:57 +0900

    🔧 テーマカラーの追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        "light-green": "#C6EDDB",
+        "light-red": "#F6D4D8",
+        "sub-red": "#C7243A",
+        "theme-emerald": "#5EC84E",
+        "theme-green": "#009250",
+        "theme-red": "#A00E0E",
+        "theme-yellow": "#F0BA32",
+      },
+    },
+  },
 };
 
 export default config;
